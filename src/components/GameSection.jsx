83dfb042561c68969ebc7f27358190f840fd63fc
kimdogbe <kimdogbe@gmail.com>
2024-12-05 14:32:07 +0000

    Changing repo name

--- src/components/GameSection.jsx
+++ src/components/GameSection.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import ArtCard from "./ArtCard";
 
 export default function GameSection({ difficulty='easy' }) {
+  const [selected, setSelected] = useState([]);
+  const [currentSelection, setCurrentSelection] = useState('');
+
   return <div className="game-section">
     <h2>Select a character you haven't picked yet</h2>
     <div className="cards">
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
+      <ArtCard />
       <ArtCard />
     </div>
   </div>
