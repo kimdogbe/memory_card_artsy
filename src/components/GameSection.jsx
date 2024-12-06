8caf1ac3a6ed16c591c43e730c57bb174119ec8e
kimdogbe <kimdogbe@gmail.com>
2024-12-06 13:31:46 +0000

    Data fetching complete

--- src/components/GameSection.jsx
+++ src/components/GameSection.jsx
@@ -2,16 +2,7 @@ import { useState, useEffect } from "react";
 import ArtCard from "./ArtCard";
 
 const endpoint = "https://api.artic.edu/api/v1/artworks/";
-const imageEndpoint = "https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg"
-
-const artists = [
-  'atta-kwami', 'salvador-dali',
-  'pablo-picasso', 'leonardo-da-vinci',
-  'ibrahim-mahama', 'vincent-van-gogh',
-  'lakwena-maciver', 'damien-hirst',
-  'banksy', 'azadeh-nia',
-  'roy-lichtenstein', 'kaws'
-]
+const imageEndpoint = "https://www.artic.edu/iiif/2/{identifier}/full/843,/0/default.jpg";
 
 const chicagoArtworks = [
   '89503', '129884', '28560', '6565', '80607', '229396',
@@ -29,13 +20,25 @@ export default function GameSection({ difficulty='easy' }) {
 
   useEffect(() => {
     const fetchUsers = async () => {
+      let tempList = []
       try {
         for (const artwork of chicagoArtworks) {
           const response = await fetch(endpoint + artwork);
           const data = await response.json();
-          setArtList([...artList, data]);
-          console.log(data);
+          const body = data.data;
+          tempList = 
+            [...tempList, 
+              { id: body.id, 
+                name: body.title,
+                artist: body.artist_title,
+                type: body.department_title,
+                image: imageEndpoint.replace('{identifier}', body.image_id)
+              }
+            ];
+          console.log(data.data);
         }
+        setArtList(tempList);
+        console.log(artList);
       } catch (err) {
         setError(err.message);
       } finally {
@@ -52,15 +55,13 @@ export default function GameSection({ difficulty='easy' }) {
   return <div className="game-section">
     <h2>Select a character you haven't picked yet</h2>
     <div className="cards">
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
-      <ArtCard />
+      {artList.map(art => <ArtCard 
+        key={art.id} 
+        artwork={art.name} 
+        artist={art.artist}
+        medium={art.type}
+        imgSrc={art.image}
+        />)}
     </div>
   </div>
 }
